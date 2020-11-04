54805f10c248fa60af355c4dbbc9219b4cca26ee
Corey Martin <coreyjmartin2@gmail.com>
2020-11-04 04:44:55 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -9,11 +9,11 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   end
   
   def change
-    # create_table :artists do |t|
-    #   t.string :name
-    #   t.string :genre
-    #   t.integer :age
-    #   t.string :hometown
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
   end
 end
